d2ac6e3257def0356ef7a49a3578e2f6fdddd91d
Filip Wasilewski <en@ig.ma>
2013-05-18 13:19:33 +0200

    Reformat file

--- run_tests.py
+++ run_tests.py
@@ -7,19 +7,20 @@ from django.conf import settings, global_settings
 
 if not settings.configured:
     settings.configure(
-        DATABASES = {
+        DATABASES={
             "default": {
                 "ENGINE": "django.db.backends.sqlite3",
             }
         },
-        INSTALLED_APPS = [
+        INSTALLED_APPS=[
             "easy_pjax",
             "tests",
         ],
-        TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS +
-            ("django.core.context_processors.request",),
-        ROOT_URLCONF ="tests.urls",
-        DEBUG = False,
+        TEMPLATE_CONTEXT_PROCESSORS=
+        global_settings.TEMPLATE_CONTEXT_PROCESSORS +
+        ("django.core.context_processors.request",),
+        ROOT_URLCONF="tests.urls",
+        DEBUG=False,
     )
 
 from django.test.simple import DjangoTestSuiteRunner
@@ -38,7 +39,9 @@ def run_tests(*test_args, **kwargs):
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("--failfast", action="store_true", default=False, dest="failfast")
-    parser.add_option("--verbosity", action="store", default=1, type=int, dest="verbosity")
+    parser.add_option("--failfast", action="store_true",
+                      default=False, dest="failfast")
+    parser.add_option("--verbosity", action="store",
+                      default=1, type=int, dest="verbosity")
     (options, args) = parser.parse_args()
     run_tests(failfast=options.failfast, verbosity=options.verbosity, *args)
