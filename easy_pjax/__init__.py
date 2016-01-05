10d0b7c452c8d9d5893cfe612e0beaa738f61628
Filip Wasilewski <en@ig.ma>
2016-01-05 23:46:48 +0100

    Add to template builtins only if add_to_buitlins is available (Django <= 1.8)

--- easy_pjax/__init__.py
+++ easy_pjax/__init__.py
@@ -10,11 +10,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 __version__ = "1.2.0"
 
-
+has_add_to_builtins = True
 try:
     from django.template import add_to_builtins
 except ImportError:
-    # import path changed in 1.8
-    from django.template.base import add_to_builtins
+    try:
+        # import path changed in 1.8
+        from django.template.base import add_to_builtins
+    except ImportError:
+        has_add_to_builtins = False
 
-add_to_builtins("easy_pjax.templatetags.pjax_tags")
+if has_add_to_builtins:
+    add_to_builtins("easy_pjax.templatetags.pjax_tags")
